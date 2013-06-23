3fb3e50a65163125946c9bf0a46846ab3a52ff8d
Louis Nyffenegger <louis.nyffenegger@gmail.com>
2013-06-23 19:05:57 +1000

    Fix SQLinjection in deletefromcart.php
    Hi mate,
    
    just a quick fix for 2 sql injections. I think this values are integer so intval is your best bet. If they are strings, mysql_real_escape_string is probably more appropriate.

--- DeleteFromCart.php
+++ DeleteFromCart.php
@@ -1,6 +1,6 @@
 <?php
-$user_id=$_GET['user_id'];
-$product_id=$_GET['product_id'];
+$user_id=intval($_GET['user_id']);
+$product_id=intval($_GET['product_id']);
 include_once "extentions/dbconnect.php";
 $delete= "DELETE  FROM tempcart_db WHERE user_id = '$user_id' AND product_id='$product_id'";
 mysql_query($delete);
